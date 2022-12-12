e0416e674fd483c4df568dc43548e5378fb0760b
Michał Bator <michalbator1996@gmail.com>
2022-12-12 11:03:30 +0100

    node vefsion

---
+++ src/local_properties.js
@@ -0,0 +1,10 @@
+const flaskService = 'http://127.0.0.1:5000/';
+const springService = 'http://127.0.0.1:8080/';
+const getFileService = 'http://192.168.0.226:6001/'
+const bookSyncCheck = 'http://127.0.0.1:5000/books/check-sync'
+const moviesSyncCheck = 'http://127.0.0.1:5000/movies/check-sync'
+export {flaskService}
+export {springService}
+export {getFileService}
+export {bookSyncCheck}
+export {moviesSyncCheck}
